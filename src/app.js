f821809c93fbe302cb9d686919b73e56ad0a774f
Michael Grinspan <mike.grinspan@gmail.com>
2021-02-14 12:44:09 -0500

    Added Sentry to the frontend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import Vue from 'vue';
+import * as Sentry from '@sentry/vue';
+import { Integrations as SentryIntegrations } from '@sentry/tracing';
 
 import Main from './components/Main';
+
+Sentry.init({
+	Vue,
+	dsn: process.env.MIX_SENTRY_DSN_FRONTEND,
+	integrations: [new SentryIntegrations.BrowserTracing()],
+	logErrors: true,
+});
 
 // Extend Vue
 const App = Vue.extend({ render: renderer => renderer(Main) });
