66c84abbbdd133a65d864a717a5f04a11bc5be30
Michael Grinspan <mike.grinspan@gmail.com>
2021-02-23 23:08:32 -0500

    polyfill for all browsers

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,13 @@
 const mix = require('laravel-mix');
 
+require('laravel-mix-polyfill');
+
 mix.js('src/app.js', 'assets')
 	.vue()
 	.sass('src/style.scss', 'assets')
 	.setPublicPath('public')
-	.version();
+	.version()
+	.polyfill({
+		enabled: true,
+		targets: '>0%',
+	});
